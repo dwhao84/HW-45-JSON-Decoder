64aa99d247c67c65e727004a16a720fff865181c
Daweihao84 <dwsamurai84@gmail.com>
2024-03-09 00:09:42 +0800

    Add fetch data fetchJoke function.

--- JSON-Decoder.playground/Pages/06_Joker API.xcplaygroundpage/Contents.swift
+++ JSON-Decoder.playground/Pages/06_Joker API.xcplaygroundpage/Contents.swift
@@ -13,7 +13,7 @@ struct Joker: Decodable {
     let error: Bool
     let category: String
     let type: String
-    let joke: String
+    let joke: String?
     let flags: Flags
     let id: Int
     let safe: Bool
@@ -30,35 +30,26 @@ struct Flags: Decodable {
 }
 
 func fetchJoke() {
-    // 假设这是您从网络获取的 JSON 数据
-    let jsonData = """
-    {
-        "error": false,
-        "category": "Misc",
-        "type": "single",
-        "joke": "Stop being homophobic and rude to the LGBTQ+ community. You should be thanking them for saving us plenty of room in heaven.",
-        "flags": {
-            "nsfw": false,
-            "religious": true,
-            "political": false,
-            "racist": false,
-            "sexist": false,
-            "explicit": false
-        },
-        "id": 114,
-        "safe": false,
-        "lang": "en"
+    guard let url = URL(string: "https://v2.jokeapi.dev/joke/Any") else {
+        print("Invalid URL")
+        return
     }
-    """.data(using: .utf8)!
-
-    do {
-        let decoder = JSONDecoder()
-        let jokeData = try decoder.decode(Joker.self, from: jsonData)
-        print(jokeData.error)
-        print(jokeData.id)
-    } catch {
-        print(error)
-    }
+    
+    URLSession.shared.dataTask(with: url) { data, response, error in
+        guard let data = data else {
+            print(error?.localizedDescription ?? "Can't get the data")
+            return
+        }
+        do {
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            let jokeData = try decoder.decode(Joker.self, from: data)
+            print(jokeData.error)
+            print(jokeData.id)
+        } catch {
+            print("Decoding error: \(error)")
+        }
+    }.resume()
 }
 
 fetchJoke()
