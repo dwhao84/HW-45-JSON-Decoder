20cb63c24f0ba18bf41e4bc7f65016fd51b4b96e
Daweihao84 <dwsamurai84@gmail.com>
2024-03-10 20:15:07 +0800

    IG API change to iTunes API.

---
+++ JSON-Decoder.playground/Pages/07_iTunes.xcplaygroundpage/Contents.swift
@@ -0,0 +1,49 @@
+/*
+ JSON Address:
+ https://itunes.apple.com/search?term=jack+johnson&limit=25
+ */
+
+import UIKit
+
+struct Itunes: Decodable {
+    let resultCount: Int
+    let results: [Results]
+}
+
+struct Results: Decodable {
+    let wrapperType: String
+    let kind: String
+    let artistName: String
+    let collectionName: String
+    let trackName: String
+    let collectionCensoredName: String
+    let trackCensoredName: String
+    let collectionArtistId: Int
+    let collectionArtistViewUrl: URL
+    let collectionViewUrl: URL
+    let trackViewUrl: URL
+    let previewUrl: URL
+    let artworkUrl30: URL
+    let artworkUrl60: URL
+    let collectionPrice: Double
+    let trackPrice: Double
+    let trackRentalPrice: Double
+    let collectionHdPrice: Double
+    let trackHdPrice: Double
+    let trackHdRentalPrice: Double
+    let releaseDate: Date
+    let collectionExplicitness: String
+    let trackExplicitness: String
+    let discCount: Int
+    let discNumber: Int
+    let trackCount: Int
+    let trackNumber: Int
+    let trackTimeMillis: Int
+    let country: String
+    let currency: String
+    let primaryGenreName: String
+    let contentAdvisoryRating: String
+    let shortDescription: String
+    let longDescription: String
+    let hasITunesExtras: Bool
+}
